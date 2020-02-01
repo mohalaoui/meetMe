1b29c70efafe3528690486de73e645b1bec2926b
malaoui <al.med@live.fr>
2020-02-01 20:15:38 +0100

    only ihm components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const appRoutes: Routes = [
+  { path: 'home',       component: HeaderComponent },
+  { path: 'about',      component: AboutComponent },
+  { path: 'services',   component: ServicesComponent },
+  { path: 'resume',     component: ResumeComponent },
+  { path: 'work',       component: PortfolioComponent },
+  { path: 'contact',    component: ContactComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**',         component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    StatisticsComponent,
+    ContactComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
